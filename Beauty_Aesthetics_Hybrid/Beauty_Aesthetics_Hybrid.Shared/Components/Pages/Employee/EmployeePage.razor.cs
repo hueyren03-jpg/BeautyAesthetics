6b431ab4e70893e98cb1152b9d5443adb9168150
hueyren03-jpg <hueyren03@gmail.com>
2026-09-21 12:52:42 +0800

    Add employee page action feedback

--- Beauty_Aesthetics_Hybrid/Beauty_Aesthetics_Hybrid.Shared/Components/Pages/Employee/EmployeePage.razor.cs
+++ Beauty_Aesthetics_Hybrid/Beauty_Aesthetics_Hybrid.Shared/Components/Pages/Employee/EmployeePage.razor.cs
@@ -1,4 +1,5 @@
 ﻿using Beauty_Aesthetics_WebPos.Components.Services.Employees;
+using Beauty_Aesthetics_WebPos.Components.Services.Feedback;
 using Beauty_Aesthetics_WebPos.Components.ViewModels;
 using Microsoft.AspNetCore.Components;
 using Microsoft.JSInterop;
@@ -10,6 +11,7 @@ namespace Beauty_Aesthetics_WebPos.Components.Pages.Employee
         [Inject] private NavigationManager NavManager { get; set; } = default!;
         [Inject] private IEmployeeService EmployeeService { get; set; } = default!;
         [Inject] private IJSRuntime JS { get; set; } = default!;
+        [Inject] private AppFeedbackService Feedback { get; set; } = default!;
 
         private EmployeePageViewModel VM { get; set; } = default!;
 
@@ -64,6 +66,7 @@ namespace Beauty_Aesthetics_WebPos.Components.Pages.Employee
             else
             {
                 EmployeeDetailsError = result.ErrorMessage ?? "Unable to load the complete employee record.";
+                Feedback.Error(EmployeeDetailsError, "Employee details unavailable");
             }
 
             IsEmployeeDetailsLoading = false;
@@ -93,11 +96,13 @@ namespace Beauty_Aesthetics_WebPos.Components.Pages.Employee
 
         private async Task NavigateToAddEmployeeAsync()
         {
+            Feedback.Info("Opening a new employee form.", "Add employee", 2200);
             await VM.NavigateToAddEmployeeAsync();
         }
 
         private async Task NavigateToEditEmployeeAsync(string code)
         {
+            Feedback.Info($"Opening employee {code} for editing.", "Edit employee", 2200);
             await VM.NavigateToEditEmployeeAsync(code);
         }
 
