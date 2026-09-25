6c221fda10091cd0191518663d0db6747601833d
hueyren03-jpg <hueyren03@gmail.com>
2026-09-25 15:45:48 +0800

    Remove unused product setup assignment registration

--- Beauty_Aesthetics_Hybrid/Beauty_Aesthetics_Hybrid/MauiProgram.cs
+++ Beauty_Aesthetics_Hybrid/Beauty_Aesthetics_Hybrid/MauiProgram.cs
@@ -80,7 +80,6 @@ namespace Beauty_Aesthetics_Hybrid
             builder.Services.AddScoped<InventoryPendingAcceptAC>();
             builder.Services.AddScoped<BranchAC>();
             builder.Services.AddScoped<IProductInventoryService, ProductInventoryService>();
-builder.Services.AddScoped<IProductSetupAssignmentService, ProductSetupAssignmentService>();
             builder.Services.AddSingleton<ProductImageService>();
             builder.Services.AddScoped<IInventoryOptionService, InventoryOptionService>();
             builder.Services.AddScoped<IStockGrnService, StockGrnService>();
